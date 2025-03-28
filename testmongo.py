6105820946b1574ca770a7fc45fc13cf7e154ec9
Heenagama Udagira - 10898926 <10898926@students.plymouth.ac.uk>
2025-03-28 14:28:25 +0530

    Changes

--- testmongo.py
+++ testmongo.py
@@ -1,5 +1,11 @@
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
 
-client = MongoClient("MONGODB_URI")
+# Load environment variables
+load_dotenv()
+MONGODB_URI = os.getenv("MONGODB_URI")
+
+client = MongoClient(MONGODB_URI)
 db = client["census"]
 print(db.list_collection_names())
