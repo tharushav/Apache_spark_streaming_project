0244e9086eba2fcaf3f2a5d41c6ee38bfcc2afbe
Heenagama Udagira - 10898926 <10898926@students.plymouth.ac.uk>
2025-03-28 14:19:26 +0530

    Changes

--- testmongo.py
+++ testmongo.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://tharushavihanga2003:NiP4a8ccTJyPGSQb@cluster0.3d8js.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient("MONGODB_URI")
 db = client["census"]
 print(db.list_collection_names())
